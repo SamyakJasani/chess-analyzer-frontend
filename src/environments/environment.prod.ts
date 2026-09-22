8ba17cf17842cf35dfc10c6b99ae07796d3d8a59
Samyak Jasani <samyak.jasani@somaiya.edu>
2026-09-22 14:02:37 +0530

    Changed URL back to domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiBaseUrl: 'https://chess-analyzer-sj-hyavg4hxebepgjgz.centralindia-01.azurewebsites.net/api',
-  apiBaseUrl: 'https://chess-analyzer.samyakjasani.dev/api',
+  apiBaseUrl: 'https://chess-analyzer-sj-hyavg4hxebepgjgz.centralindia-01.azurewebsites.net/api',
 };
