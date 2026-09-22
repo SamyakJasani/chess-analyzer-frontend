17d908091fe8c222072238620ee9bc249afd6920
Samyak Jasani <samyak.jasani@somaiya.edu>
2026-09-22 14:01:41 +0530

    Chnaged URL to custom domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://chess-analyzer-sj-hyavg4hxebepgjgz.centralindia-01.azurewebsites.net/api',
+  // apiBaseUrl: 'https://chess-analyzer-sj-hyavg4hxebepgjgz.centralindia-01.azurewebsites.net/api',
+  apiBaseUrl: 'https://chess-analyzer.samyakjasani.dev/api',
 };
